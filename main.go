26425950077d2464798fa9236a436fa4c586c407
Miguel A. <miguelsantosamorim3@gmail.com>
2025-11-21 21:35:47 -0300

    Update menu options and add shopping list feature

--- main.go
+++ main.go
@@ -12,7 +12,9 @@ func menu() {
 	fmt.Println("===  CONTROLE DE GASTOS ===")
 	fmt.Println("1 - adicionar gasto")
 	fmt.Println("2 - ver gastos")
-	fmt.Println("3 - definir ou redefinir salario mensal(crucial)")
+	fmt.Println("3 - lista de compras")
+	fmt.Println("4 - ver salario")
+	fmt.Println("5 - definir ou redefinir salario mensal(crucial)")
 	fmt.Println("0 - sair")
 	fmt.Println("===========================")
 	fmt.Print("opcao escolhida: ")
@@ -27,10 +29,17 @@ func adicionarGastos() {
 	var quantidade int
 	fmt.Scanln(&quantidade)
 
-	if salario == 0 {
-		fmt.Println("parece que voce nao definiu seu salario!")
+	//tratamento de entrada simples
+	if quantidade < 0 {
+		fmt.Println("erro: quantidade menor que 0")
 		return
 	}
+
+	if salario == 0 {
+		fmt.Println("erro: nao definiu o salario")
+		return
+	}
+
 	for i := 1; i <= quantidade; i++ {
 		fmt.Println("gasto numero ", i)
 
@@ -62,6 +71,13 @@ func redefinir() {
 	if definiu == false {
 		fmt.Print("defina seu salario mensal(quantos ganha por mes): ")
 		fmt.Scanln(&ganha_em_mes)
+
+		//tratamento de entrada basico
+		if ganha_em_mes < 0 {
+			fmt.Println("valor menor que 0!")
+			return
+		}
+
 		salario = ganha_em_mes
 		fmt.Println("definido!")
 		definiu = true
@@ -70,11 +86,50 @@ func redefinir() {
 		fmt.Print("redefina seu salario mensal: ")
 		antigo = salario
 		fmt.Scanln(&ganha_em_mes)
-		salario = ganha_em_mes
 
+		//tratamento de entrada basico
+		if ganha_em_mes < 0 {
+			fmt.Println("valor menor que 0!")
+			return
+		}
+
+		salario = ganha_em_mes
 		fmt.Println("novo salario: ", salario)
 		fmt.Println("antigo salario: ", antigo)
 	}
+}
+
+func compras() {
+	var nome_produto string
+	var preco_produto float64
+	var quantidade_compras int
+	var preco_final_compras float64
+
+	fmt.Println("coloque itens na sua lista")
+
+	for {
+		fmt.Print("qual o nome do produto: ")
+		fmt.Scanln(&nome_produto)
+		fmt.Print("qual o valor do produto(0.00 para encerrar a lista de compras): ")
+		fmt.Scanln(&preco_produto)
+
+		fmt.Println("==============================================================")
+
+		if preco_produto == 0.00 {
+			break
+		}
+
+		quantidade_compras += 1
+
+		preco_final_compras += preco_produto
+	}
+	fmt.Println("===  ESTASTISTICAS DA COMPRA ===")
+	fmt.Println("itens: ", quantidade_compras)
+	fmt.Println("preço total dos produtos: ", preco_final_compras)
+}
+
+func separador() {
+	fmt.Println("================================")
 }
 
 func main() {
@@ -83,7 +138,7 @@ func main() {
 		menu()
 		fmt.Scanln(&opcao)
 
-		fmt.Println("===========================")
+		separador()
 		switch opcao {
 		case 0:
 			fmt.Println("saindo...")
@@ -93,11 +148,16 @@ func main() {
 		case 2:
 			verGastos()
 		case 3:
+			compras()
+		case 4:
+			fmt.Println("salario: ", salario)
+		case 5:
 			redefinir()
 		default:
 			fmt.Println("opcao invalida")
 		}
-		fmt.Println("===========================")
+		separador()
 
 	}
 }
+
